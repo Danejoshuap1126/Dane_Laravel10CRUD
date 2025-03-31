0b7bebc1e9c8b646c04989ef5ba8d5123c6fffc1
Danejoshuap1126 <occ.dane.joshua@gmail.com>
2025-03-31 15:30:59 +0800

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); // Added missing closing parenthesis
+
+Route::resource('products', App\Http\Controllers\ProductController::class);
